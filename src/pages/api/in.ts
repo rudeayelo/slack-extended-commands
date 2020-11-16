fcd3dceaa9665abe1396339d4ac39870a2fbe1a0
Rude <rude.ayelo@xing.com>
2020-11-16 15:56:05 +0100

    feat: add `in` command

---
+++ src/pages/api/in.ts
@@ -0,0 +1,60 @@
+import { cors } from "src/utils/cors";
+import { SlackApiClient } from "src/slack-api";
+
+export default async (req, res) => {
+  await cors(req, res);
+
+  const { channel_id, team_id, user_id, text } = req.body;
+
+  const slackApi = await SlackApiClient({ userId: user_id, teamId: team_id });
+
+  /* ---------------------- Post message to the channel --------------------- */
+
+  if (text) {
+    try {
+      await slackApi.chat.postMessage({
+        channel: channel_id,
+        as_user: true,
+        text,
+      });
+    } catch (error) {
+      console.error("--> ERROR posting the message", error);
+      throw new Error(error);
+    }
+  }
+
+  /* -------------------------- Remove custom status ------------------------ */
+
+  try {
+    await slackApi.users.profile.set({
+      // @ts-ignore
+      profile: {
+        status_text: "",
+        status_emoji: "",
+      },
+    });
+  } catch (error) {
+    console.error("--> ERROR setting custom status:", error);
+  }
+
+  /* --------------------------- Mark as available -------------------------- */
+
+  try {
+    await slackApi.users.setPresence({ presence: "auto" });
+  } catch (error) {
+    console.error("--> ERROR setting user's presence:", error);
+  }
+
+  /* ----------------------- Toggle off Do Not Disturb ---------------------- */
+
+  try {
+    await slackApi.dnd.endDnd();
+  } catch (error) {
+    console.error("--> ERROR setting Do Not Disturb porperty:", error);
+  }
+
+  /* ------------------------------------------------------------------------ */
+
+  res.setHeader("Content-Type", "application/json");
+  res.status(200).end();
+};
